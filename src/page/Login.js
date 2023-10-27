29cb695206b0c9fd52ff9093880ea94a5fa62b56
Tuckkrub <6322770817@g.siit.tu.ac.th>
2023-10-28 00:15:40 +0700

    Changes to be committed: 	new file:   src/page/Login.css 	modified:   src/page/Login.js 	deleted:    src/pages/login.css 	deleted:    src/pages/login.js

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,16 +1,37 @@
-import { Outlet, Link } from "react-router-dom";
-import './Layout.css'
+import React, { useState } from "react";
+import "./Layout.css";
+import "./Login.css"
 
 
 const Login = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    function togglePasswordVisibility() {
+        setPasswordVisible(!passwordVisible);
+    }
+
     return (
-      <div>
-        <h1>Login</h1>
-        <Link className="link" to="/registers">Registers</Link>
-      </div>
-    )
-      
-  };
-  
-  export default Login;
-  
+        <div className="centered-component">
+            <div>
+                <h1>Login</h1>
+
+                <div className="input-container">
+                    <h3>USERNAME:</h3>
+                    <input type="text" id="userInput" name="userInput" />
+                </div>
+                <div className="input-container">
+                    <h3 >PASSWORD: </h3>
+                    <input
+                        type={passwordVisible ? "text" : "password"}
+                        id="passInput"
+                        name="passInput"
+                    />
+                    <input type="checkbox" onClick={togglePasswordVisibility} />
+                    <p>Show password</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
